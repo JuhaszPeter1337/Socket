22522b69eff1a2c7924a1da9663e9cfa37512db5
Peter Juhasz <juhasz.peter1337@gmail.com>
2023-09-14 22:39:35 +0200

    Changed comments

--- connect.py
+++ connect.py
@@ -1,8 +1,8 @@
 import socket
 
 # socket.AF_INET     : This specifies that the socket will use the IPv4 address family.
-# socket.AF_INET6    : This specifies that the socket will use the IPv6 address family
-# socket.SOCK_STREAM : This specifies that the socket will use the TCP protocol,
+# socket.AF_INET6    : This specifies that the socket will use the IPv6 address family.
+# socket.SOCK_STREAM : This specifies that the socket will use the TCP protocol.
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 result = sock.connect_ex(('localhost', 8888))
